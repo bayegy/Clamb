e34e1a49f34ffc82d44e4c8708c1b445edf41b27
unknown <947366452@qq.com>
2019-03-17 23:03:19 +0800

    second version of weipu

--- utils/net.py
+++ utils/net.py
@@ -10,37 +10,48 @@ class Net(object):
     def __init__(self):
         self.driver = None
 
-    def getProxy(self):
+    def get_proxy(self):
         print("Activing foxDriver, always remember to close Net.driver if you don't use getProxy anymore.")
-        self.driver = self.foxDriver()
+        self.driver = self.fox_driver()
         while True:
-            self.driver.get('http://www.goubanjia.com/')
-            res = self.driver.page_source
-            tree = html.fromstring(res)
-            ipnode = tree.xpath("//td[@class='ip']")
-            ips = []
-            for ipdn in range(len(ipnode)):
-                ip = ipnode[ipdn].xpath("*[not(@style='display:none;' or @style='display: none;')]/text()")
-                ip = "".join(ip[0:-1]) + ":" + ip[-1]
-                ips.append(ip)
-            # ip2=tree.xpath("//td[@class='ip']//text()")
-            ln = len(ips)
-            tp = np.array(tree.xpath("//tbody/tr/td[not(@class='ip')]/a/text()")).reshape(ln, 6)
-            for n in range(ln):
-                if tp[n, 0] == '高匿' and tp[n, 1] == 'http':
-                    ipout = {}
-                    ipout['http'] = "http://" + ips[n]
-                    yield(ipout)
+            try:
+                self.driver.get('http://www.goubanjia.com/')
+                res = self.driver.page_source
+                tree = html.fromstring(res)
+                ipnode = tree.xpath("//td[@class='ip']")
+                ips = []
+                for ipdn in range(len(ipnode)):
+                    ip = ipnode[ipdn].xpath("*[not(@style='display:none;' or @style='display: none;')]/text()")
+                    ip = "".join(ip[0:-1]) + ":" + ip[-1]
+                    ips.append(ip)
+                # ip2=tree.xpath("//td[@class='ip']//text()")
+                ln = len(ips)
+                tp = np.array(tree.xpath("//tbody/tr/td[not(@class='ip')]/a/text()")).reshape(ln, 6)
+                for n in range(ln):
+                    if tp[n, 0] == '高匿' and tp[n, 1] == 'http':
+                        ipout = {}
+                        ipout['http'] = "http://" + ips[n]
+                        yield(ipout)
+            except Exception as e:
+                print(e)
+                yield(self.get_proxy())
 
-    def parseForm(self, file, sep):
+    def parse_form(self, file, sep):
         d = {}
-        for line in open(file, "r"):
-            li = re.split(sep, line.strip())
-            if len(li) == 2:
-                d[li[0]] = li[1]
-        return(d)
+        with open(file, "r",encoding="utf-8") as f:
+            for line in f:
+                li = re.split(sep, line)
+                if len(li) == 2:
+                    d[li[0].strip()] = li[1].strip()
+            return(d)
 
-    def foxDriver(self):
+    def fox_driver(self):
         option = webdriver.FirefoxOptions()
         option.set_headless()
         return webdriver.Firefox(firefox_options=option)
+
+    def close(self):
+        try:
+            self.driver.close()
+        except Exception:
+            pass
